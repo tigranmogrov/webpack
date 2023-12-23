110342c4365bee5dce1acb94a571e137d37533ad
Tigran <tigranmogrov@gmail.com>
2023-12-23 17:04:06 +0200

    feat: add PurgeCSSPlugin and add settings

--- config/plugins.js
+++ config/plugins.js
@@ -5,7 +5,10 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 const fs = require('fs');
+const { PurgeCSSPlugin } = require('purgecss-webpack-plugin');
+const { glob } = require('glob');
 const IS_HASH = process.env.NODE_ENV === 'hash';
+const IS_CSS_PURE = process.env.NODE_ENV === 'pure-css';
 
 /*-------------------*/
 
@@ -16,38 +19,36 @@ const DEFAULTS = { description: 'description', keywords: 'keywords' };
 /* Created methods HtmlWebpackPlugins */
 
 const GetHtmlWebpackPlugins = (dir, ext, chunkName) => {
+  const files = fs.readdirSync(path.resolve(__dirname, dir));
 
-    const files = fs.readdirSync(path.resolve(__dirname, dir));
+  const results = [];
 
-    const results = [];
+  files.map((item) => {
+    const parts = item.split('.');
+    const name = parts[0];
+    const extension = parts[1];
+    if (!extension || !name) return;
+    const filename = `${dir}/${name}.${ext === 'ejs' ? 'html' : ext}`;
+    const options = {
+      filename: filename.replace('../src/', ''),
+      template: path.resolve(__dirname, `${dir}/${name}.${ext}`),
+      hash: false,
+      chunks: [`${name}`, 'main'],
+      inject: 'body',
+      scriptLoading: 'blocking',
+      minify: {
+        collapseWhitespace: true,
+      },
+    };
+    if (chunkName) {
+      options.chunks = chunkName.split(' ');
+    }
+    options.excludeChunks = ['assets', 'parts'];
 
-    files.map(item => {
-        const parts = item.split('.');
-        const name = parts[0];
-        const extension = parts[1];
-        if (!extension || !name) return;
-        const filename = `${dir}/${name}.${ext === 'ejs' ? 'html' : ext}`;
-        const options = {
-            filename: filename.replace('../src/', ''),
-            template: path.resolve(__dirname, `${dir}/${name}.${ext}`),
-            hash: false,
-            chunks: [`${name}`,'main'] ,
-            inject: 'body',
-            scriptLoading: 'blocking',
-            minify: {
-                collapseWhitespace: true,
-            },
-        };
-        if (chunkName) {
-            options.chunks = chunkName.split(' ');
-        }
-        options.excludeChunks = ['assets', 'parts'];
+    results.push(new HtmlWebpackPlugin({ ...DEFAULTS, ...options }));
+  });
 
-        results.push(new HtmlWebpackPlugin({ ...DEFAULTS, ...options }));
-    });
-
-    return results;
-
+  return results;
 };
 
 /*-------------------*/
@@ -56,46 +57,47 @@ const GetHtmlWebpackPlugins = (dir, ext, chunkName) => {
 
 const indexChunk = GetHtmlWebpackPlugins('../src', 'html');
 
-const allHtmlChunk = [...indexChunk,];
+const allHtmlChunk = [...indexChunk];
 
 /*-------------------*/
 
 module.exports = {
+  common: [
+    ...allHtmlChunk,
+    new MiniCssExtractPlugin({ filename: `assets/styles/[name]${IS_HASH ? '.[contenthash]' : ''}.css` }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'src/assets/images', to: 'assets/images/' }],
+    }),
+  ],
 
-    common: [
-        ...allHtmlChunk,
-        new MiniCssExtractPlugin({ filename: `assets/styles/[name]${IS_HASH ? '.[contenthash]' : ''}.css`, }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'src/assets/images', to: 'assets/images/' },
-            ],
-        }),
-    ],
+  dev: [],
 
-    dev: [],
+  prod: [
+    Boolean(IS_CSS_PURE) &&
+      new PurgeCSSPlugin({
+        paths: glob.sync(`${path.join(__dirname, '../src')}/**/*`, { nodir: true }),
+      }),
+  ],
 
-    prod: [],
-
-    minimizer: [
-        new CssMinimizerPlugin({
-            minimizerOptions: {
-                preset: [
-                    'default',
-                    {
-                        discardComments: { removeAll: true },
-                    },
-                ],
-            },
-        }),
-        new TerserPlugin({
-            extractComments: false,
-            minify: TerserPlugin.uglifyJsMinify,
-            terserOptions: {
-                compress: {
-                    pure_funcs: ['console.log'],
-                },
-            },
-        })
-    ]
-
+  minimizer: [
+    new CssMinimizerPlugin({
+      minimizerOptions: {
+        preset: [
+          'default',
+          {
+            discardComments: { removeAll: true },
+          },
+        ],
+      },
+    }),
+    new TerserPlugin({
+      extractComments: false,
+      minify: TerserPlugin.uglifyJsMinify,
+      terserOptions: {
+        compress: {
+          ['pure_funcs']: ['console.log'],
+        },
+      },
+    }),
+  ],
 };
